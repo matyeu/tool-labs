4f8e42fe90b123812ee3a0ffddac00b71d6795aa
matyeu <mathieuletutour@gmail.com>
2023-10-28 18:45:21 +0200

    Import guildMemberRemove

---
+++ src/Events/Guild/guildMemberRemove.ts
@@ -0,0 +1,14 @@
+import { GuildMember } from "discord.js";
+import { ToolClient } from "../../Library";
+import { find } from "../../Models/member";
+
+const Logger = require("../../Library/logger");
+
+export default async function (client: ToolClient, oldMember: GuildMember) {
+
+    const memberConfig: any = await find(oldMember.guild!.id, oldMember.user.id);
+    
+    if (memberConfig) await memberConfig.delete();
+
+    await Logger.client(`${oldMember.user.tag} has just left ${oldMember.guild.name}`);
+}
